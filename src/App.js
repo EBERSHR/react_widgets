cb1c98a5411e03fc2e6236a3311a2e61e58d37ae
Eber Hernández <ebershr@gmail.com>
2021-12-12 00:29:59 -0500

    Componente SerialLights

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import './App.css';
 import Container from './components/Container';
+import Seriallights from './components/SerialLights';
 
 function App() {
   return (
     <div className="App">
       Hola widget!!!
           <Container />
+          <Seriallights />
     </div>
   );
 }
---
+++ src/components/SerialLights.jsx
@@ -0,0 +1,30 @@
+import React from 'react';
+// const serialPort = require('serialport');
+
+
+const Seriallights = () => {
+    // const port = new serialPort(
+    //     "/dev/ttyACM0",
+    //     {
+    //         baudRate: 9600
+    //     }
+    // );
+    
+    // const parser = new serialPort.parsers.Readline();
+
+    // port.pipe(parser);
+    
+    // parser.on('data', (line) => {
+    //     console.log(line)
+    //     port.write('era una vez ')
+    // })
+
+
+    return (
+        <div>
+            
+        </div>
+    );
+}
+
+export default Seriallights;
